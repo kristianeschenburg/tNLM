6221ff1aa6bbc8a8c32e85d22d3f29174df25ff1
Kristian Eschenburg <keschenb@uw.edu>
2018-02-20 18:22:00 -0800

    process tnlm

---
+++ bin/apply_tNLM.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 18:13:33 2018
+
+@author: kristianeschenburg
+"""
+
+import os
+import sys
+sys.path.append('../../niIO/')
+sys.path.append('../../shortestpath/')
+sys.path.append('../../tNLM/')
+
+import niIO
+import shortestpath
+import tNLM
+
+import networkx as nx
+import scipy.io as sio
+
+if __name__ == "__main__":
+    
+    mainDir = '/mnt/parcellator/parcellation/parcellearning/Data/'
+    subjectList = ''.join([mainDir,'Subjects.txt'])
+    
+    with open(subjectList,'r') as inSubj:
+        subjects = inSubj.readlines()
+    subjects = [x.strip() for x in subjects]
+    
+    cutoff = 10
+    h = 0.7
+    
+    for subj in subjects:
+        
+        restFile = ''.join([mainDir,'RestingState/',subj,'.rfMRI_Z-Trans_merged_CORTEX_LEFT.mat'])
+        surfFile = ''.join([mainDir,'Surfaces/',subj,'.L.inflated.32k_fs_LR.surf.gii'])
+        outFile = ''.join([mainDir,'RestingState/',subj,'.L.TNLM.mat'])
+        
+        if os.path.exists(restFile) and os.path.exists(surfFile):
+            
+            rest = niIO.loaded.loadMat(restFile)
+            S = shortestpath.SurfaceAdjacency(surfFile)
+            S.generate_adjList()
+            G = nx.from_dict_of_lists(S.adj_list)
+            
+            apsp = nx.all_pairs_shortest_path_length(G,cutoff=cutoff)
+            apsp = dict(apsp)
+            
+            accepted = [k for k in apsp.keys() if apsp.keys()]
+            APSP = {k: apsp[k].keys() for k in accepted}
+            
+            smoothed = tNLM.smooth(rest,APSP,h)
+            smooth = {'tnlm': smoothed}
+            sio.savemat(outFile,smooth)
+    
